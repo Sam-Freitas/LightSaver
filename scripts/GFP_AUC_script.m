f3efe164f9712d7d2d5029cb3d0069a292a3272e
Sam Freitas <60946332+SamFreitas1996@users.noreply.github.com>
2021-02-22 14:09:53 -0700

    script update 2/21/2021

--- scripts/GFP_AUC_script.m
+++ scripts/GFP_AUC_script.m
@@ -1,10 +1,5 @@
-% Created by Samuel Freitas
-% Feb 18th 2021
-% University of Arizona 
-% Sutphin Lab
-
 close all
-img_dir_path = "C:\Users\Lab PC\Documents\GFP_AUC\data\Raul_data\2021-02-16.1\Exported\";
+img_dir_path = "C:\Users\Lab PC\Documents\GFP_AUC\data\Raul_data\2021-02-16\Exported\";
 
 show_output_images = 0;
 
