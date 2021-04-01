d4c75ad7dae6cff10c8f724eba72ae7b98071e12
Sam Freitas <samfreitas@email.arizona.edu>
2021-04-01 13:44:28 -0700

    reverted changes

--- scripts/testing/GFP_AUC_script_testing_gauss.m
+++ scripts/testing/GFP_AUC_script_testing_gauss.m
@@ -35,11 +35,11 @@ for i = 1:length(img_paths)
     if z>3
         this_img = this_img(:,:,1:3);
     end
-        
+    
     % Split channles
     R = this_img(:,:,1); G = this_img(:,:,2); B = this_img(:,:,3);
     
-    % find the dominant color of the fluorescence 
+    % find the dominant color of the fluorescence
     [~,color_choice] = max([sum(R(:)),sum(G(:)),sum(B(:))]);
     
     % Remove 1mm bar from our microscope images
@@ -55,66 +55,100 @@ for i = 1:length(img_paths)
             data = B - R - G;
     end
     
-    % this assumes that all the data is in the uint8 format
-    data_norm = double(data)/255;
+    se = strel('disk',100);
+    background = imopen(data,se);
     
-    % setp through consequitive iterations of a threshold based off the
-    % mean and std of the image intensities
-    for j = 1:6
-        % create a threshold
-        this_thresh = mean2(data_norm)+(std2(data_norm)*(1/5)*(j-1));
-        % create a mask
-        this_mask = imgaussfilt(data_norm,2)>this_thresh;
-        % remove any small blobs from the mask
-        this_mask = bwareaopen(this_mask,3000);
-        % label the mask 
+    filt_data = double(data).*(data>background);
+    
+    lin_data = nonzeros(double(data).*(filt_data>median(filt_data(:))));
+    
+    counts = hist(lin_data,max(lin_data));
+    counts = medfilt1(counts,3);
+    
+    x = 1:length(counts);
+    
+    TF = islocalmin(counts);
+    
+    local_mins = x(TF);
+    
+    try
+        this_mask = data>local_mins(1);
+        this_mask = bwareaopen(this_mask,1000);
+        
         this_label = bwlabel(this_mask);
         
-        % if there are 5 blobs in the mask
         if max(this_label(:)) == number_worms_to_detect
-            % step one iteration further
-            this_thresh2 = mean2(data_norm)+(std2(data_norm)*(1/5)*(j));
-            this_mask2 = imgaussfilt(data_norm,2)>this_thresh2;
-            this_mask2 = bwareaopen(this_mask2,3000);
-            this_label2 = bwlabel(this_mask2);
+            disp(['Found ' num2str(number_worms_to_detect) ' blobs'])
+        else
+            disp('trying again')
+            error('trying mathmatical method')
+        end
+
+    catch
+        % this assumes that all the data is in the uint8 format
+        data_norm = double(data)/255;
+        
+        % setp through consequitive iterations of a threshold based off the
+        % mean and std of the image intensities
+        for j = 1:6
+            % create a threshold
+            this_thresh = mean2(data_norm)+(std2(data_norm)*(1/5)*(j-1));
+            % create a mask
+            this_mask = imgaussfilt(data_norm,2)>this_thresh;
+            % remove any small blobs from the mask
+            this_mask = bwareaopen(this_mask,3000);
+            % label the mask
+            this_label = bwlabel(this_mask);
             
-            % if there are still 5 blobs then keep this mask 
-            if max(this_label2(:)) == number_worms_to_detect
-                this_mask = this_mask2;
-                this_label = this_label2;
+            % if there are 5 blobs in the mask
+            if max(this_label(:)) == number_worms_to_detect
+                % step one iteration further
+                this_thresh2 = mean2(data_norm)+(std2(data_norm)*(1/5)*(j));
+                this_mask2 = imgaussfilt(data_norm,2)>this_thresh2;
+                this_mask2 = bwareaopen(this_mask2,3000);
+                this_label2 = bwlabel(this_mask2);
                 
-                % break out of the loop
+                % if there are still 5 blobs then keep this mask
+                if max(this_label2(:)) == number_worms_to_detect
+                    this_mask = this_mask2;
+                    this_label = this_label2;
+                    
+                    % break out of the loop
+                    break
+                end
+                % break out of the loop if there are 5 blobs
                 break
             end
-            % break out of the loop if there are 5 blobs 
-            break
+            
         end
-        
     end
     
-    % thicken all the masks 
+    
+    
+    
+    % thicken all the masks
     new_mask = bwmorph(this_mask,'Thicken',1);
     % close small edges and zones
     new_mask = imclose(new_mask,strel('disk',5));
-    % fill the holes 
+    % fill the holes
     new_mask = imfill(new_mask,'holes');
-    % re-label the masks 
+    % re-label the masks
     labeled_masks = bwlabel(new_mask);
     
     % mask the inital data without the normalization step
-    % gets rid of background signals 
-    masked_data = new_mask.*double(data); 
+    % gets rid of background signals
+    masked_data = new_mask.*double(data);
     
-    % integrate the entire signal across the masks and only the masks  
+    % integrate the entire signal across the masks and only the masks
     for j = 1:max(labeled_masks(:))
         this_labeled_mask = double(data).*(labeled_masks==j);
-        image_integral_intensities(i,j) = sum(sum(this_labeled_mask.*double(data)));
-        image_integral_area(i,j) = sum(sum((this_labeled_mask.*double(data))>0));
+        image_integral_intensities(i,j) = sum(sum(this_labeled_mask));
+        image_integral_area(i,j) = sum(sum((this_labeled_mask)>0));
         
-%         if image_integral_area(i,j)>20000
-%             image_integral_area(i,j) = 0;
-%             image_integral_intensities(i,j) = 0;
-%         end
+        %         if image_integral_area(i,j)>20000
+        %             image_integral_area(i,j) = 0;
+        %             image_integral_intensities(i,j) = 0;
+        %         end
     end
     
     % converts the labeled mask to RGB (easier to read)
@@ -126,15 +160,15 @@ for i = 1:length(img_paths)
     imwrite(imtile({this_img,rgb_labeled_mask,masked_data_output},'GridSize',[1,3]),...
         fullfile(output_path,[img_paths(i).name '_img' num2str(i) '.png']))
     
-    % show the sequence if necessary 
+    % show the sequence if necessary
     if show_output_images == 1
         imshow(imtile({this_img,rgb_labeled_mask,masked_data_output},'GridSize',[1,3]),[]);
         title([img_paths(i).name ' -- img ' num2str(i)], 'Interpreter', 'none');
     end
     
     linear_data = nonzeros(masked_data);
-        
-    [counts,binLoc] = hist(linear_data,255); 
+    
+    [counts,binLoc] = hist(linear_data,255);
     
 end
 
