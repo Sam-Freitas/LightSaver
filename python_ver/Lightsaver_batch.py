22b05d6df0ee0d11d3d226d5332c4955ded883ac
Sam Freitas <60946332+Sam-Freitas@users.noreply.github.com>
2024-04-30 14:49:59 -0700

    Update Lightsaver_batch.py
    Former-commit-id: fd0029d43f150c13e39ddd692f986d0a51c1f504 [formerly 2d666ee48085d121a02d8d9eccc940d16f4911b0]
    Former-commit-id: dd3ac6c868d3e05609471c00f75648f3ab00f63e
    Former-commit-id: 09cd86def871ced42cdf2abba1b9d2829d987895

--- python_ver/Lightsaver_batch.py
+++ python_ver/Lightsaver_batch.py
@@ -1,10 +1,29 @@
 import tkinter as tk
 from tkinter import simpledialog, filedialog
+from fnmatch import fnmatch
+from natsort import natsorted
 import os
 
 # Suppress warnings
 import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
+
+def clean_img_names(img_paths):
+
+    cleaned_file_names = [os.path.basename(path) for path in img_paths]
+    common_prefix = os.path.commonprefix(cleaned_file_names)
+    cleaned_file_names = [file_name[len(common_prefix):] for file_name in cleaned_file_names]
+
+    common_suffix = os.path.commonprefix([w[::-1] for w in cleaned_file_names])[::-1]
+    cleaned_file_names = [file_name[:-len(common_suffix)] for file_name in cleaned_file_names]
+
+    counter = 0
+    for i,this_name in enumerate(cleaned_file_names):
+        if this_name in img_paths[i]:
+            counter += 1
+    assert counter == len(img_paths)
+
+    return cleaned_file_names
 
 def get_directory():
     root = tk.Tk()
@@ -13,18 +32,32 @@ def get_directory():
     # Make the window always on top
     root.attributes('-topmost', True)
 
+    # get the os a current path for data selections
     detected_opterating_system = os.name
+    current_path = os.getcwd()
 
-    # Determine the default documents folder based on the operating system
-    if detected_opterating_system == 'posix':  # Linux or macOS
-        initial_dir = os.path.expanduser("~/Documents")
-    elif detected_opterating_system == 'nt':   # Windows
-        initial_dir = os.path.join(os.path.expanduser("~"), "Documents")
-    else:
-        initial_dir = os.getcwd()  # Fallback to current directory
+    # find where the lightsaver script is being ran from for data selection
+    idx_of_lightsvr = current_path.rfind('LightSaver')
 
+    # if in the defualt folders then do nothing
+    if idx_of_lightsvr != -1:
+        initial_dir = os.path.join(current_path[:idx_of_lightsvr+11],'data')
+    else: # if not the defualt to 'documents' folder 
+        # Determine the default documents folder based on the operating system
+        if detected_opterating_system == 'posix':  # Linux or macOS
+            initial_dir = os.path.expanduser("~/Documents")
+        elif detected_opterating_system == 'nt':   # Windows
+            initial_dir = os.path.join(os.path.expanduser("~"), "Documents")
+        else:
+            initial_dir = os.getcwd()  # Fallback to current directory
+    
+    # make the output path for all the images
+    output_path = os.path.join(current_path[:idx_of_lightsvr+11],'exported_images')
+
+    # use the gui to make the data selection
     directory = filedialog.askdirectory(initialdir=initial_dir)
-    return directory
+
+    return directory, output_path
 
 def get_user_inputs():
     root = tk.Tk()
@@ -69,11 +102,6 @@ def get_user_inputs():
 # Get user inputs
 inputs = get_user_inputs()
 
-# Print out the inputs
-print("User inputs:")
-for input_ in inputs:
-    print(input_)
-
 # This is for the exported images
 # Faster is with the jpg format -> 0 but less quality on the images
 high_quality_output = 1
@@ -85,5 +113,31 @@ else:
 # Get current path
 curr_path = os.getcwd()
 
-selected_directory = get_directory()
+# get the selected directory and the first part of the output path for the exported images
+selected_directory, output_path = get_directory()
 print("Selected directory:", selected_directory)
+
+# get the final save name of the experiment and create the output directory for it
+final_save_name = os.path.split(selected_directory)[-1]
+output_path = os.path.join(output_path,final_save_name)
+os.makedirs(output_path,exist_ok=True)
+
+# step through all the files in the selected data folder to get a list of the tif images paths
+pattern = "*.tif"
+img_paths = []
+
+# find all the image files and then natsort them
+for path, subdirs, files in os.walk(selected_directory):
+    for name in files:
+        if fnmatch(name, pattern):
+            img_paths.append(os.path.join(path, name))
+img_paths = natsorted(img_paths)
+
+print('There are',img_paths.__len__(),'files with the *.tif ests.\n')
+
+if img_paths.__len__() == 0:
+    exit("No TIF images found")
+
+img_names = clean_img_names(img_paths)
+
+print('eof')
